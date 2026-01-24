de07d294854a1c0c3f1ccd755a91c9e76f4e9c31
Pwootage <pwootage@gmail.com>
2026-01-24 12:00:32 -0700

    Remove the old python version (keep it locally, though, just in case)

--- python/patch.bat
+++
@@ -1,2 +0,0 @@
-main.exe iso -i prime2.iso -o metroid-prime2-practice-mod.iso -m prime-practice
-pause
--- python/patch_dev.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash -xe
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-cd "${SCRIPT_DIR}"
-PYTHONPATH="${SCRIPT_DIR}" python3 main.py iso --skip-hash -i prime2-orig.iso -o metroid-prime2-practice-mod.iso -m ./prime-practice
